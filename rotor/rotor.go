9ac0096dbbd82ba3caf1b778aba36cbc927734f9
Bruce Ediger <bediger8@gmail.com>
2024-12-25 17:15:22 -0700

    Now with all 5 rotors, generated by mkrotor

--- rotor/rotor.go
+++ rotor/rotor.go
@@ -74,11 +74,16 @@ func (r *Rotor) CipherBkwd(inPos int, verbose bool) (outPos int) {
 }
 
 /*
-Entry = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+Entry = ABCDEFGHIJKLMNOPQRSTUVWXYZ (rotor right side)
         ||||||||||||||||||||||||||
 I     = EKMFLGDQVZNTOWYHXUSPAIBRCJ
+II    = AJDKSIRUXBLHWTMCQGZNPYFVOE
+III   = BDFHJLCPRTXVZNYEIWGAKMUSQO
+IV    = ESOVPZJAYQUIRHXLNFTGKDCMWB
+V     = VZBRGITYUPSDNHLXAWMJQOFECK
 */
-var Rotor1 = &Rotor{
+
+var RotorI = &Rotor{
 	Encode: [26]int{
 		'E' - 'A', 'K' - 'A', 'M' - 'A', 'F' - 'A', 'L' - 'A',
 		'G' - 'A', 'D' - 'A', 'Q' - 'A', 'V' - 'A', 'Z' - 'A',
@@ -95,4 +100,84 @@ var Rotor1 = &Rotor{
 		'R' - 'A', 'I' - 'A', 'N' - 'A', 'Q' - 'A', 'O' - 'A',
 		'J' - 'A',
 	},
+}
+var RotorII = &Rotor{
+	Encode: [26]int{
+		'A' - 'A', 'J' - 'A', 'D' - 'A', 'K' - 'A', 'S' - 'A',
+		'I' - 'A', 'R' - 'A', 'U' - 'A', 'X' - 'A', 'B' - 'A',
+		'L' - 'A', 'H' - 'A', 'W' - 'A', 'T' - 'A', 'M' - 'A',
+		'C' - 'A', 'Q' - 'A', 'G' - 'A', 'Z' - 'A', 'N' - 'A',
+		'P' - 'A', 'Y' - 'A', 'F' - 'A', 'V' - 'A', 'O' - 'A',
+		'E' - 'A',
+	},
+	Inverse: [26]int{
+		'A' - 'A', 'J' - 'A', 'P' - 'A', 'C' - 'A', 'Z' - 'A',
+		'W' - 'A', 'R' - 'A', 'L' - 'A', 'F' - 'A', 'B' - 'A',
+		'D' - 'A', 'K' - 'A', 'O' - 'A', 'T' - 'A', 'Y' - 'A',
+		'U' - 'A', 'Q' - 'A', 'G' - 'A', 'E' - 'A', 'N' - 'A',
+		'H' - 'A', 'X' - 'A', 'M' - 'A', 'I' - 'A', 'V' - 'A',
+		'S' - 'A',
+	},
+}
+var RotorIII = &Rotor{
+	Encode: [26]int{
+		'B' - 'A', 'D' - 'A', 'F' - 'A', 'H' - 'A', 'J' - 'A',
+		'L' - 'A', 'C' - 'A', 'P' - 'A', 'R' - 'A', 'T' - 'A',
+		'X' - 'A', 'V' - 'A', 'Z' - 'A', 'N' - 'A', 'Y' - 'A',
+		'E' - 'A', 'I' - 'A', 'W' - 'A', 'G' - 'A', 'A' - 'A',
+		'K' - 'A', 'M' - 'A', 'U' - 'A', 'S' - 'A', 'Q' - 'A',
+		'O' - 'A',
+	},
+	Inverse: [26]int{
+		'T' - 'A', 'A' - 'A', 'G' - 'A', 'B' - 'A', 'P' - 'A',
+		'C' - 'A', 'S' - 'A', 'D' - 'A', 'Q' - 'A', 'E' - 'A',
+		'U' - 'A', 'F' - 'A', 'V' - 'A', 'N' - 'A', 'Z' - 'A',
+		'H' - 'A', 'Y' - 'A', 'I' - 'A', 'X' - 'A', 'J' - 'A',
+		'W' - 'A', 'L' - 'A', 'R' - 'A', 'K' - 'A', 'O' - 'A',
+		'M' - 'A',
+	},
+}
+var RotorIV = &Rotor{
+	Encode: [26]int{
+		'E' - 'A', 'S' - 'A', 'O' - 'A', 'V' - 'A', 'P' - 'A',
+		'Z' - 'A', 'J' - 'A', 'A' - 'A', 'Y' - 'A', 'Q' - 'A',
+		'U' - 'A', 'I' - 'A', 'R' - 'A', 'H' - 'A', 'X' - 'A',
+		'L' - 'A', 'N' - 'A', 'F' - 'A', 'T' - 'A', 'G' - 'A',
+		'K' - 'A', 'D' - 'A', 'C' - 'A', 'M' - 'A', 'W' - 'A',
+		'B' - 'A',
+	},
+	Inverse: [26]int{
+		'H' - 'A', 'Z' - 'A', 'W' - 'A', 'V' - 'A', 'A' - 'A',
+		'R' - 'A', 'T' - 'A', 'N' - 'A', 'L' - 'A', 'G' - 'A',
+		'U' - 'A', 'P' - 'A', 'X' - 'A', 'Q' - 'A', 'C' - 'A',
+		'E' - 'A', 'J' - 'A', 'M' - 'A', 'B' - 'A', 'S' - 'A',
+		'K' - 'A', 'D' - 'A', 'Y' - 'A', 'O' - 'A', 'I' - 'A',
+		'F' - 'A',
+	},
+}
+var RotorV = &Rotor{
+	Encode: [26]int{
+		'V' - 'A', 'Z' - 'A', 'B' - 'A', 'R' - 'A', 'G' - 'A',
+		'I' - 'A', 'T' - 'A', 'Y' - 'A', 'U' - 'A', 'P' - 'A',
+		'S' - 'A', 'D' - 'A', 'N' - 'A', 'H' - 'A', 'L' - 'A',
+		'X' - 'A', 'A' - 'A', 'W' - 'A', 'M' - 'A', 'J' - 'A',
+		'Q' - 'A', 'O' - 'A', 'F' - 'A', 'E' - 'A', 'C' - 'A',
+		'K' - 'A',
+	},
+	Inverse: [26]int{
+		'Q' - 'A', 'C' - 'A', 'Y' - 'A', 'L' - 'A', 'X' - 'A',
+		'W' - 'A', 'E' - 'A', 'N' - 'A', 'F' - 'A', 'T' - 'A',
+		'Z' - 'A', 'O' - 'A', 'S' - 'A', 'M' - 'A', 'V' - 'A',
+		'J' - 'A', 'U' - 'A', 'D' - 'A', 'K' - 'A', 'G' - 'A',
+		'I' - 'A', 'A' - 'A', 'R' - 'A', 'P' - 'A', 'H' - 'A',
+		'B' - 'A',
+	},
+}
+
+var Rotors = map[string]*Rotor{
+	"I":   RotorI,
+	"II":  RotorII,
+	"III": RotorIII,
+	"IV":  RotorIV,
+	"V":   RotorV,
 }
