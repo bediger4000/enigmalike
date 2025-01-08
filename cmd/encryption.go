c8ef04df25b3f6fc0f5c283322356296758ac0a4
Bruce Ediger <bediger8@gmail.com>
2025-01-07 21:54:32 -0700

    encryption using a self-contained object

---
+++ cmd/encryption.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"enigmalike/enigma"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	inFileName := flag.String("i", "", "input file name")
+	first := flag.String("1", "I", "first rotor")
+	second := flag.String("2", "II", "second rotor")
+	third := flag.String("3", "III", "third rotor")
+	settings := flag.String("S", "AAA", "initial rotor settings")
+	flag.Parse()
+
+	var cleartext string
+
+	if *inFileName != "" {
+		if buffer, err := os.ReadFile(*inFileName); err != nil {
+			log.Fatal(err)
+		} else {
+			cleartext = string(buffer)
+		}
+	} else {
+		// no input file, read a string from the command line
+		if flag.NArg() > 0 {
+			cleartext = flag.Arg(0)
+		}
+	}
+
+	machine := enigma.NewMachine(*first, *second, *third)
+	machine.SetRotors(*settings)
+
+	for _, letter := range cleartext {
+		if letter < 'A' || letter > 'Z' {
+			continue
+		}
+		cipherLetter := machine.EncryptLetter(letter)
+		fmt.Printf("%c", cipherLetter)
+	}
+	fmt.Println()
+}
